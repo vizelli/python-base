aae32a63d8e347e5d8c25c873d91414d6012af50
Alan Vizelli <alanwv@hotmail.com>
2023-11-10 12:39:44 -0300

    Formatado tabela de Tabuada

--- tabuada.py
+++ tabuada.py
@@ -14,13 +14,15 @@ Tabuada do 2:
 6
 ...
 """
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 __author__ = "Alan"
 
 numeros = list(range(1, 11))
 
-for numero in numeros:
-    print("Tabuada do numero:",numero)
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-    print("------------")
+for n1 in numeros:
+    print("{:-^18}".format(f"Tabuada do numero: {n1}"))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+    print("#" * 18)
