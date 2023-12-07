5443df7a59cd76217e2579b8e324697ea873ffd6
Alan Vizelli <alanwv@hotmail.com>
2023-12-07 11:24:35 -0300

    Script melhorado

--- hello.py
+++ hello.py
@@ -21,8 +21,29 @@ __author__ = "Alan Willer Vizelli"
 __license__ = "Unlicense"
 
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5]
+arguments = {"lang": None, "count": 1}
+
+for arg in sys.argv[1:]:
+  # TODO: Tratar ValueError
+  key, value = arg.split("=")
+  key = key.lstrip("-").strip()
+  value = value.strip()
+  if key not in arguments:
+    print(f"Invalid Option `{key}´")
+    sys.exit()
+  arguments[key] = value
+
+current_language = arguments["lang"]
+if current_language is None:
+    # TODO: Usar repetição
+    if "LANG" in os.environ:
+      current_laguage = os.getenv("LANG")
+    else:
+      current_language = input("Choose a language:")
+
+current_language = current_language[:5]
 
 msg = {
   "en_US": "Hello, World!",
@@ -31,4 +52,6 @@ msg = {
   "fr_FR": "Bonjour, Monde!",
 }
 
-print(msg[current_language])
+print(
+  msg[current_language] * int(arguments["count"])
+)
